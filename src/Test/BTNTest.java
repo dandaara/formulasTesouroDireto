8a924eae8ef1c2d30d4a9541f148b1b9ed4d67f5
gitl <gitl>
2017-05-10 17:01:20 -0300

    Teste falho de BTN

---
+++ src/Test/BTNTest.java
@@ -0,0 +1,14 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BTNTest {
+
+	@Test
+	public void testPrecoUnitarioAtualizado(){
+		fail("Not yet implemented");
+	}
+
+}
