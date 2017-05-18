7b23d23f38433e02a7e91dd686201bef9dac7a18
Dandara Aranha <dandaraaranha@gmail.com>
2017-05-17 21:24:24 -0300

    Implementação do método de calcular juros com testes passando

--- src/CadernoFormulasSelic/BTN.java
+++ src/CadernoFormulasSelic/BTN.java
@@ -1,5 +1,8 @@
 package CadernoFormulasSelic;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class BTN {
 
 	private float PUant;
@@ -11,8 +14,14 @@ public class BTN {
     private float numeroMeses; 
 	
 	
+	public float calculaPrecoUnitario() {
+		float precoUnitario = PUant * TR;
+		setPrecoUnitario(precoUnitario);
+		return precoUnitario;
+	}
+	
 	public float getPrecoUnitario() {
-		return (PUant * TR);
+		return this.precoUnitario;
 	}
 
 
@@ -45,17 +54,39 @@ public class BTN {
 		
 	}
 
-
 	public void setNumeroMeses(int numeroMeses) {
 		this.numeroMeses = numeroMeses; 
+	}
+
+	public float getJuros() {
+		return getFatorJuros()*getPrecoUnitario(); 
+	}
+	
+	public float getFatorJuros() {
+        return calculaFatorJuros();
+    }
+	
+	public void setTaxaJuros(float taxa) {
+		this.taxaJuros = taxa; 
+		
+	}
+	
+	 private float calculaFatorJuros(){
+		 
+		 float divisaoTaxa =(this.taxaJuros/100.0f)+1.0f;
+		 float potencia = this.numeroMeses/12;
+		 float resultadoPotencia = (float)Math.pow(divisaoTaxa, potencia);
+		 float fatorJuros = resultadoPotencia -1;
+		 
+		 setFatorJuros(fatorJuros);
+		 return fatorJuros; 
+	        
+	  
+	 }
+	 
+	 private void setFatorJuros(float fatorJuros2) {
+		this.fatorJuros = fatorJuros2;
 		
 	}
 
-
-	public double getJuros() {
-		return 2.000000;
-	}
-
-
-	
 }
--- src/Test/BTNTest.java
+++ src/Test/BTNTest.java
@@ -21,36 +21,52 @@ public class BTNTest {
 	public void testPrecoUnitarioAtualizado(){
 		btn.setTR(0.100000f);
 		btn.setPUant(0.200000f);
-		assertEquals(0.020000f, btn.getPrecoUnitario(), 0.000001f);
+		assertEquals(0.020000f, btn.calculaPrecoUnitario(), 0.000001f);
 	}
 	
 	   @Test
 	    public void testPrecoUnitarioAtualizadoValoresAleatorios(){
 	        btn.setTR(0.038800f);
 	        btn.setPUant(1.965789f);
-	        assertEquals(0.076272f, btn.getPrecoUnitario(), 0.000001f);
+	        assertEquals(0.076272f, btn.calculaPrecoUnitario(), 0.000001f);
 	    }
 	    
 	    @Test
 	    public void testPrecoUnitarioAtualizadoValoresMaioresQueZero(){
 	        btn.setTR(1.000000f);
 	        btn.setPUant(2.000000f);
-	        assertEquals(2.000000f, btn.getPrecoUnitario(), 0.000001f);
+	        assertEquals(2.000000f, btn.calculaPrecoUnitario(), 0.000001f);
 	    }
 	    
 	    @Test
 	    public void testPrecoUnitarioAtualizadoValoresIguaisAZero(){
 	        btn.setTR(0.000000f);
 	        btn.setPUant(2.000000f);
-	        assertEquals(0.000000f, btn.getPrecoUnitario(), 0.000001f);
+	        assertEquals(0.000000f, btn.calculaPrecoUnitario(), 0.000001f);
 	    }
 	    
 	    @Test
 	    public void testJuros(){
 	        btn.setPrecoUnitario(2.000000f);
-	        btn.setJuros(1.000000f);
+	        btn.setTaxaJuros(1.000000f);
 	        btn.setNumeroMeses(6);
-	        assertEquals(2.000000, btn.getJuros(), 0.000001f);
+	        assertEquals(0.009975, btn.getJuros(), 0.000001f);
+	    }
+	    
+	    @Test
+	    public void testJurosZero(){
+	        btn.setPrecoUnitario(0.000000f);
+	        btn.setTaxaJuros(1.000000f);
+	        btn.setNumeroMeses(6);
+	        assertEquals(0.000000, btn.getJuros(), 0.000001f);
+	    }
+	    
+	    @Test
+	    public void testJurosMaiorQueUM(){
+	        btn.setPrecoUnitario(300.000000f);
+	        btn.setTaxaJuros(800.000000f);
+	        btn.setNumeroMeses(12);
+	        assertEquals(2400.000000, btn.getJuros(), 0.000001f);
 	    }
 	    
 	    
