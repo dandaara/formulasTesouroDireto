492f4fcab0782dc3203672c54dcecc100cb55ee1
Edson Gomes <edinhogomes@gmail.com>
2017-05-21 18:21:56 -0300

    Teste do LFT_A passando

--- src/Test/LFT_ATest.java
+++ src/Test/LFT_ATest.java
@@ -5,17 +5,21 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import CadernoFormulasSelic.LFT_A;
+
 public class LFT_ATest {
+	
+	LFT_A lfta;
 
 	@Before
 	public void setUp() throws Exception {
-		LFT_A lfta = new LFT_A(0.000537464, 23.098761, 0.0245, 5, 10, 180, 100);
+		lfta = new LFT_A(0.000537464f, 23.098761f, 5, 10, 180, 100);
 		
 	}
 
 	@Test
 	public void test() {
-		assertEquals(0.01085123, lfta.getVNa());
+		assertEquals(0.01085123, lfta.getVNa(), 0.00000001);
 	}
 
 }
