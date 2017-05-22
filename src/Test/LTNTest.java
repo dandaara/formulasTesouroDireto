f020743eb724ef5b790fedc23bab8faaff4f1f18
Edson Gomes <edinhogomes@gmail.com>
2017-05-22 02:49:39 -0300

    Retirando import nao utilizado

--- src/Test/LTNTest.java
+++ src/Test/LTNTest.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
