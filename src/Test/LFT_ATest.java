ff835ff0a192b708c4320a44541aa4ce5a577660
Edson Gomes <edinhogomes@gmail.com>
2017-05-22 00:58:03 -0300

    Metodo getVNa alterado. Teste continua passando LFT_A

--- src/Test/LFT_ATest.java
+++ src/Test/LFT_ATest.java
@@ -13,13 +13,13 @@ public class LFT_ATest {
 
 	@Before
 	public void setUp() throws Exception {
-		lfta = new LFT_A(0.000537464f, 23.098761f, 5, 10, 180, 100);
+		lfta = new LFT_A(23.098761f, 0.000537464f, 5, 10, 180, 100);
 		
 	}
 
 	@Test
-	public void test() {
-		assertEquals(0.01085123, lfta.getVNa(), 0.00000001);
+	public void testLTF_AValorNominal() {
+		assertEquals(0.01225919f, lfta.getVNa(), 0.00000001);
 	}
 
 }
