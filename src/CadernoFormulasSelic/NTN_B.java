ded4421edf88ea713278ea0b1dd608861315dcaf
Edson Gomes <edinhogomes@gmail.com>
2017-05-22 14:54:13 -0300

    Alterando metodo NTN_B

--- src/CadernoFormulasSelic/NTN_B.java
+++ src/CadernoFormulasSelic/NTN_B.java
@@ -2,8 +2,31 @@ package CadernoFormulasSelic;
 
 public class NTN_B {
 	
+	private float IPCAt;
+	private float IPCAt0;
+	private float IPCAt1;
+	private float VN;
+	
+	private int dc;
+	private int dct;
+	
+	public NTN_B(float iPCAt, float iPCAt0, float iPCAt1, float vN, int dc, int dct) {
+		this.IPCAt = iPCAt;
+		this.IPCAt0 = iPCAt0;
+		this.IPCAt1 = iPCAt1;
+		this.VN = vN;
+		this.dc = dc;
+		this.dct = dct;
+	}
+	
 	public float getVNA(){
-		return 1.022231f;
+		
+		return calculaFator() * (float) (IPCAt/IPCAt0) * VN;
+		
+	}
+
+	private float calculaFator() {
+		return (float) Math.pow((float)IPCAt1/IPCAt0, (float)dc/dct);
 	}
 
 }
