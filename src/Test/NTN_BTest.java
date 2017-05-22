69ccc1ce9f0ff66da37d8ae0147840d475e00af8
Edson Gomes <edinhogomes@gmail.com>
2017-05-22 20:25:14 -0300

    Escrevendo teste parametrizado

--- src/Test/NTN_BTest.java
+++ src/Test/NTN_BTest.java
@@ -10,6 +10,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
+import com.sun.org.apache.xpath.internal.ExtensionsProvider;
+
 import CadernoFormulasSelic.NTN_B;
 import junit.framework.Assert;
 
@@ -28,7 +30,16 @@ public class NTN_BTest {
 	
 	float expected;
 	
-	public NTN_BTest(float iPCAt, float iPCAt0, float iPCAt1, float vN, int dc, int dct, float expected) {
+	float VNA_juros;
+	float i_juros;
+	int n_juros;
+	int dcp_juros;
+	int dct_juros;
+	float expected_juros;
+	
+	
+	public NTN_BTest(float iPCAt, float iPCAt0, float iPCAt1, float vN, int dc, int dct, float expected,
+			float vna, float i_juros, int n_juros, int dcp_juros, int dct_juros, float expected_juros) {
 		this.IPCAt = iPCAt;
 		this.IPCAt0 = iPCAt0; 
 		this.IPCAt1 = iPCAt1;
@@ -36,22 +47,36 @@ public class NTN_BTest {
 		this.dc = dc;
 		this.dct = dct;
 		this.expected = expected;
+		
+		this.VNA_juros = vna;
+		this.i_juros = i_juros;
+		this.n_juros = n_juros;
+		this.dcp_juros = dcp_juros;
+		this.dct_juros = dct_juros;
+		this.expected_juros = expected_juros;
+		
+		
 	}
 	
 	@Parameters
 	public static Iterable dados() {
 		return Arrays.asList(new Object[][]{
-			{5.71f, 4.99f, 5.25f, 12.90f, 30, 23, 15.772394f},
-			{4.00f, 4.00f, 4.00f, 123.99f, 15, 20, 123.990000f},
-			{3.00f, 4.57f, 5.67f, 112.00f, 10, 10, 91.219965f},
-			{7.12f, 6.56f, 5.67f, 0.99f, 5, 15, 1.023539f}
+			{5.71f, 4.99f, 5.25f, 12.90f, 30, 23, 15.772394f, 
+				12.123456f, 8f,	6,	15,	10, 0.720365f},
+			{4.00f, 4.00f, 4.00f, 123.99f, 15, 20, 123.990000f,
+					5000.456777f, 9f, 12, 3, 10, 130.963965f},
+			{3.00f, 4.57f, 5.67f, 112.00f, 10, 10, 91.219965f,
+						1.642357f, 10f,	2,	5,	5, 0.026297f},
+			{7.12f, 6.56f, 5.67f, 0.99f, 5, 15, 1.023539f,
+							0.987766f, 1.99f, 12, 3, 4, 0.014706f}
 			
 		});
 	}
 
 	@Before
 	public void setUp() throws Exception {
-		ntn_b = new NTN_B(IPCAt,IPCAt0, IPCAt1, VN, dc, dct);
+		ntn_b = new NTN_B(IPCAt,IPCAt0, IPCAt1, VN, dc, dct, 
+				VNA_juros,i_juros, n_juros, dcp_juros, dct_juros);
 	}
 
 	@Test
@@ -61,7 +86,7 @@ public class NTN_BTest {
 	
 	@Test
 	public void testJuros(){
-		assertEquals(0.123123f, ntn_b.getJuros(), 0.000001f);
+		assertEquals(expected_juros, ntn_b.getJuros(), 0.000001f);
 	}
 
 }
