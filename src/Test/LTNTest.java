ec87afc7c81a4dec4d6a793f1295cffc4a66134d
Edson Gomes <edinhogomes@gmail.com>
2017-05-22 02:23:16 -0300

    Falsificando o teste II (Ajustando o teste que na verdade não estava passado)

--- src/Test/LTNTest.java
+++ src/Test/LTNTest.java
@@ -5,15 +5,20 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import CadernoFormulasSelic.LTN;
+
 public class LTNTest {
+	
+	LTN ltn;
 
 	@Before
 	public void setUp() throws Exception {
+		ltn = new LTN();
 	}
 
 	@Test
 	public void testPrecoUnitario() {
-		assertEquals(0.001, LTN.getPU(), 0,000001);;
+		assertEquals(0.000123f, ltn.getPrecoUnitario(), 0.000001);
 	}
 
 }
