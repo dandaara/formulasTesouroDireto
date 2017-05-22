d03aa38ff16d981e924d797a7d54d1176e30d1f8
Edson Gomes <edinhogomes@gmail.com>
2017-05-22 02:48:55 -0300

    Triangulacao utilizando parametrizacao

--- src/Test/LTNTest.java
+++ src/Test/LTNTest.java
@@ -2,23 +2,45 @@ package Test;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import CadernoFormulasSelic.LTN;
-
+@RunWith(Parameterized.class)
 public class LTNTest {
 	
 	LTN ltn;
+	float tInd;
+	float expected;
+	
+	public LTNTest(float tInd, float expected) {
+		this.tInd = tInd;
+		this.expected = expected;
+	}
 
+	@Parameters
+	public static Iterable dados(){
+		return Arrays.asList(new Object[][]{
+			{10.20f, 990.458849f},
+			{0.35f, 998.809817f},
+			{1.99f, 995.663108f}
+		});
+	}
+	
 	@Before
 	public void setUp() throws Exception {
-		ltn = new LTN(10.10);
+		ltn = new LTN(tInd);
 	}
 
 	@Test
 	public void testPrecoUnitario() {
-		assertEquals(0.000123f, ltn.getPrecoUnitario(), 0.000001);
+		assertEquals(expected, ltn.getPrecoUnitario(), 0.000001);
 	}
 
 }
